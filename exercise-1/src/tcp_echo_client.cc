8c152453d3c06756bc920886e4114b3a82671e83
sed9871 <dashwinaravind1@gmail.com>
2026-05-15 00:14:10 +0530

    Added the ability ot send any message from the command line arguments

--- exercise-1/src/tcp_echo_client.cc
+++ exercise-1/src/tcp_echo_client.cc
@@ -6,10 +6,10 @@
 #include <sys/types.h>
 #include <unistd.h>
 
-int main() {
+int main(int argc, char* argv[]) {
   // #Question - are these the same type?
   // Not all the data types below are of the same type if that was the question, If that wasn't well, std::string and C stringd are not the same data type as std::string is an implementation of a class while C style strings are arrays of chars
-  std::string message = "Hello from client";
+  std::string message = argv[1];
   const int kPort = 8080;
   const std::string kServerAddress = "127.0.0.1";
   sockaddr_in address;
